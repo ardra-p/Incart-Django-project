3ba0d64aa876afd7a235b2ca8a87b10cb0b190b3
Ardra p <165270747+ardra-p@users.noreply.github.com>
2025-06-22 22:45:47 +0530

    initial commit

---
+++ incart/products/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    LIVE =1
+    DELETE = 0
+    DELETE_CHOICES = ((LIVE,'Live'),(DELETE,'Delete'))
+    Title = models.CharField(max_length=200)
+    price = models.FloatField()
+    description = models.TextField()
+    images = models.ImageField(upload_to='media/')
+    priority = models.IntegerField(default=0)
+    detele_status = models.IntegerField(choices=DELETE_CHOICES, default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self) -> str:
+        return self.Title
